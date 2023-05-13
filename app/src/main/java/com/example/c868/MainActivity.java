40a47c64b1b8fd37f9a7f46e2cfbf1edb4f90c9d
ccarter1990 <ccar491@wgu.edu>
2023-05-13 11:58:27 -0500

    C868

--- app/src/main/java/com/example/c868/MainActivity.java
+++ app/src/main/java/com/example/c868/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //testsss
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
