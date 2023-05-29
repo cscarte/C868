b9188f23c6d36378a9a48eec788035195062c0d8
ccarter1990 <ccar491@wgu.edu>
2023-05-29 12:14:11 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/C868/DAO/AssemblyPartsDAO.java
    #	app/src/main/java/com/example/C868/Database/MRPDatabase.java
    #	app/src/main/java/com/example/C868/Database/Repository.java
    #	app/src/main/java/com/example/C868/Entity/AssemblyParts.java
    #	app/src/main/java/com/example/C868/UI/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/C868/UI/PurchasedComponentDetails.java
+++ app/src/main/java/com/example/C868/UI/PurchasedComponentDetails.java
@@ -35,9 +35,24 @@ public class PurchasedComponentDetails extends AppCompatActivity implements Adap
         purchasedComponentName.setText(name);
 
         //Get purchased component part description from clicked item in list on the PurchasedComponentList.java screen
-        purchasedComponentDescription = findViewById(R.id.editTextViewPurchasedComponentPrice);
+        purchasedComponentDescription = findViewById(R.id.editTextViewPurchasedComponentQuantity);
         description = getIntent().getStringExtra("partDescription");
         purchasedComponentDescription.setText(description);
+
+        //Get purchased component part price from clicked item in list on the PurchasedComponentList.java screen
+        purchasedComponentPrice = findViewById(R.id.editTextViewPurchasedComponentQuantity);
+        price = getIntent().getStringExtra("partPrice");
+        purchasedComponentPrice.setText(price);
+
+        //Get purchased component part quantity from clicked item in list on the PurchasedComponentList.java screen
+        purchasedComponentQuantity = findViewById(R.id.editTextViewPurchasedComponentQuantity);
+        quantity = getIntent().getStringExtra("partQuantity");
+        purchasedComponentQuantity.setText(quantity);
+
+        //Get purchased component part vendor from clicked item in list on the PurchasedComponentList.java screen
+        purchasedComponentVendor = findViewById(R.id.editTextViewPurchasedComponentLeadTime);
+        vendor = getIntent().getStringExtra("partVendor");
+        purchasedComponentVendor.setText(vendor);
     }
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
