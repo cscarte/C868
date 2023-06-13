5829e84b4541847457fa49ca11872e2c3ead5cf1
ccarter1990 <ccar491@wgu.edu>
2023-06-12 20:13:00 -0500

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/C868/UI/AssemblyTransactions.java
+++ app/src/main/java/com/example/C868/UI/AssemblyTransactions.java
@@ -1,12 +1,15 @@
 package com.example.C868.UI;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.C868.Database.Repository;
 import com.example.C868.Entity.AssemblyParts;
 import com.example.C868.Entity.PurchasedComponents;
 import com.example.c868.R;
@@ -14,18 +17,21 @@ import com.example.c868.R;
 import java.util.List;
 
 public class AssemblyTransactions extends AppCompatActivity {
+
+    Repository repository = new Repository(getApplication());
     TextView assemblyNameTransaction;
     TextView assemblyQtyOnHand;
 
     TextView qtyOnHand;
 
-    List<AssemblyParts> assemblyPartsList;
-    List<PurchasedComponents> purchasedComponentsList;
+    List<AssemblyParts> assemblyPartsList = repository.getmAllAssemblyParts();
+    List<PurchasedComponents> purchasedComponentsList = repository.getmAllPurchasedComponents();
 
     AssemblyParts assemblyParts;
     PurchasedComponents purchasedComponents;
 
     Spinner assemblySpinner;
+    int spinnerDefaultPosition = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,7 +41,19 @@ public class AssemblyTransactions extends AppCompatActivity {
         assemblyNameTransaction = findViewById(R.id.assemblyTransactionNameTextView);
         assemblyQtyOnHand = findViewById(R.id.assemblyTransactionQtyOnHandTextView);
         qtyOnHand = findViewById(R.id.assemblyTransactionQtyOnHandTextView);
-
         assemblySpinner = findViewById(R.id.assemblyNameTransactionSpinner);
+
+
+        final ArrayAdapter<AssemblyParts> assemblySpinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, assemblyPartsList);
+        assemblySpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        assemblySpinner.setAdapter(assemblySpinnerAdapter);
+
+        if (assemblyPartsList.isEmpty()) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Please add an assembly part before attempting to add a transaction.", Toast.LENGTH_SHORT);
+            toast.show();
+        } else {
+            spinnerDefaultPosition = assemblySpinner.getSelectedItemPosition();
+            assemblyParts = assemblyPartsList.get(spinnerDefaultPosition);
+        }
     }
 }
